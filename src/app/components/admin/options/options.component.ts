556ac23ede3d3e6c1f2a1b5b67f6f0353d4ff619
01szak <kacperolszewski2006@gmail.com>
2025-04-17 19:02:59 +0200

    changed tittle and favicon

--- src/app/components/admin/options/options.component.ts
+++ src/app/components/admin/options/options.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import {MatCard} from '@angular/material/card';
+import {MatInput, MatLabel} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-options',
-  imports: [],
+  imports: [
+    MatCard,
+    MatInput,
+    MatLabel,
+    FormsModule
+  ],
   templateUrl: './options.component.html',
   standalone: true,
   styleUrl: './options.component.css'
